d1066c1a485f7c83fc895a83cd5e2910a6290422
Maia <evina666@gmail.com>
2018-08-01 18:20:59 +0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,12 +1,21 @@
+ST = 'quit'
 loop {
-	puts "Input a, b, c"
-	a = gets.chomp
-	b =gets.chomp
-	c = gets.chomp
-	break if [a, b, c].include? 'quit'
-	puts a
-	puts b 
-	puts c
-	
+    puts "Input a, b, c"
+    a = gets.chomp
+    break if a.downcase == ST
+    a = a.to_f
+    b =gets.chomp
+    break if b.downcase == ST
+    c = gets.chomp
+    break if c.downcase == ST
+    c = c.to_f
+    case b
+        when '+' then puts a + с
+        when '-'then puts a - с
+        when '*' then puts a * c
+        when ('/' && c.nonzero?) then puts a / c
+        when ()
+        else print "Unrecognized sign"
+    end
 	
 }
