c0659f87073cd43db73e3b913c1d3914bf73cb7d
Maia <evina666@gmail.com>
2018-08-02 23:42:44 +0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,22 +1,36 @@
-ST = 'quit'
+def is_quit?(x)
+  x.downcase == 'quit'
+end
+
 loop {
-    puts "Input a, b, c"
-    a = gets.chomp
-    break if a.downcase == ST
-    a = a.to_f
-    b =gets.chomp
-    break if b.downcase == ST
-    c = gets.chomp
-    break if c.downcase == ST
-    c = c.to_f
-    case b
-        when '+' then puts a + c
-        when '-'then puts a - c
-        when '*' then puts a * c
-        when '/' then if c.nonzero? puts a / c 
-        	else puts 'division by zero'
-        end
-        else puts "Unrecognized sign '#{b}'"
+  puts 'Input a, b, c'
+
+  a = gets.chomp
+  break if is_quit? a
+  a = a.to_f
+
+  b = gets.chomp
+  break if is_quit? b
+
+  c = gets.chomp
+  break if is_quit? c
+  c = c.to_f
+
+  case b
+  when '+' then
+    puts a + c
+  when '-' then
+    puts a - c
+  when '*' then
+    puts a * c
+  when '/' then
+    if c.nonzero?
+      puts a / c
+    else
+      puts 'division by zero'
     end
-	
+  else
+    puts "Unrecognized sign '#{b}'"
+  end
+
 }
